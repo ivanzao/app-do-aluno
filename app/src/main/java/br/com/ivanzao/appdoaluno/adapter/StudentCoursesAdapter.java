adc4c28a069557a206ac986546d6d897faa2ce30
ivanzao <ivan.assalim@gmail.com>
2017-11-08 03:42:27 -0200

    Adding regular grades to data card

--- app/src/main/java/br/com/ivanzao/appdoaluno/adapter/StudentCoursesAdapter.java
+++ app/src/main/java/br/com/ivanzao/appdoaluno/adapter/StudentCoursesAdapter.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import br.com.ivanzao.appdoaluno.R;
 import br.com.ivanzao.appdoaluno.student.CourseData;
+import br.com.ivanzao.appdoaluno.student.Grade;
 
 public class StudentCoursesAdapter extends ArrayAdapter<CourseData> {
 
@@ -27,7 +28,6 @@ public class StudentCoursesAdapter extends ArrayAdapter<CourseData> {
         this.elements = (ArrayList<CourseData>) objects;
     }
 
-    @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         CourseData course = elements.get(position);
@@ -40,9 +40,15 @@ public class StudentCoursesAdapter extends ArrayAdapter<CourseData> {
         TextView txtSubject = dataCard.findViewById(R.id.title);
         txtSubject.setText(course.getName());
 
-        TextView txtCode = dataCard.findViewById(R.id.code);
-        txtCode.setText(course.getCode());
+        populateAverages(dataCard, course);
+        populateAbsences(dataCard, course);
+        populateGrades(dataCard, course.getGrades());
+        populateEdpGrades(dataCard, course.getEdpGrades());
 
+        return dataCard;
+    }
+
+    private void populateAverages(View dataCard, CourseData course) {
         String partialAverage = (course.getPartialAverage() != null) ?
                 String.valueOf(course.getPartialAverage()) :
                 "Não consta";
@@ -54,8 +60,68 @@ public class StudentCoursesAdapter extends ArrayAdapter<CourseData> {
                 "Não consta";
         TextView txtFinalAverage = dataCard.findViewById(R.id.finalAverage);
         txtFinalAverage.setText(finalAverage);
+    }
 
-        return dataCard;
+    private void populateAbsences(View dataCard, CourseData course) {
+        String absences = (course.getAbsences() != null) ?
+                String.valueOf(course.getAbsences()) :
+                "Não consta";
+        TextView txtAbsences = dataCard.findViewById(R.id.abscences);
+        txtAbsences.setText(absences);
+
+        String absencesLimit = (course.getAbsencesLimit() != null) ?
+                String.valueOf(course.getAbsencesLimit()) :
+                "Não consta";
+        TextView txtAbsencesLimit = dataCard.findViewById(R.id.abscencesLimit);
+        txtAbsencesLimit.setText(absencesLimit);
+    }
+
+    private void populateGrades(View dataCard, List<Grade> grades) {
+        if (grades != null && grades.size() > 0) {
+            dataCard.findViewById(R.id.gradeTitle).setVisibility(View.VISIBLE);
+            dataCard.findViewById(R.id.grade0).setVisibility(View.VISIBLE);
+
+            Grade grade0 = grades.get(0);
+            TextView txtGradeDate0 = dataCard.findViewById(R.id.gradeDate0);
+            txtGradeDate0.setText(grade0.getDate().substring(5));
+
+            String mark0 = (grade0.getMark() != null) ?
+                    String.valueOf(grade0.getMark()) :
+                    "N/C";
+            TextView txtGradeMark0 = dataCard.findViewById(R.id.gradeMark0);
+            txtGradeMark0.setText(mark0);
+
+            if (grades.size() > 1) {
+                dataCard.findViewById(R.id.grade1).setVisibility(View.VISIBLE);
+
+                Grade grade1 = grades.get(1);
+                TextView txtGradeDate1 = dataCard.findViewById(R.id.gradeDate1);
+                txtGradeDate1.setText(grade1.getDate().substring(5));
+
+                String mark1 = (grade1.getMark() != null) ?
+                        String.valueOf(grade1.getMark()) :
+                        "N/C";
+                TextView txtGradeMark1 = dataCard.findViewById(R.id.gradeMark1);
+                txtGradeMark1.setText(mark1);
+            }
+
+            if (grades.size() > 2) {
+                dataCard.findViewById(R.id.grade2).setVisibility(View.VISIBLE);
+
+                Grade grade2 = grades.get(2);
+                TextView txtGradeDate2 = dataCard.findViewById(R.id.gradeDate2);
+                txtGradeDate2.setText(grade2.getDate().substring(5));
+
+                String mark2 = (grade2.getMark() != null) ?
+                        String.valueOf(grade2.getMark()) :
+                        "N/C";
+                TextView txtGradeMark2 = dataCard.findViewById(R.id.gradeMark2);
+                txtGradeMark2.setText(mark2);
+            }
+        }
+    }
+
+    private void populateEdpGrades(View dataCard, List<Grade> edpGrades) {
     }
 
 }
